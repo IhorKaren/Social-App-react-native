2adaf6edf75c749156f5fe89c6c3ffbb04acfa75
Ihor Karen <karenihor@gmail.com>
2023-06-04 16:12:14 +0300

    hw-2

---
+++ App.jsx
@@ -0,0 +1,27 @@
+import { StatusBar } from "expo-status-bar";
+import { useFonts } from "expo-font";
+import React from "react";
+import PostsScreen from "./Screens/PostsScreen";
+import LoginScreen from "./Screens/LoginScreen";
+import RegistrationScreen from "./Screens/RegistrationScreen";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <>
+      <StatusBar style="auto" />
+      {/* <PostsScreen /> */}
+      {/* <LoginScreen /> */}
+      <RegistrationScreen />
+    </>
+  );
+}
