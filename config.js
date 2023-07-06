bc2f6c39e005919a05c31fc2504269477a96b9ca
Ihor <karenihor@gmail.com>
2023-07-06 20:29:30 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,17 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDXrm9m1ngXdLAivso8T2nLlfTBnGgh9xw",
-  authDomain: "socialapp-19676.firebaseapp.com",
-  databaseURL:
-    "https://socialapp-19676-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "socialapp-19676",
-  storageBucket: "socialapp-19676.appspot.com",
-  messagingSenderId: "724460431073",
-  appId: "1:724460431073:web:381038ab9318c827b079a0",
-  measurementId: "G-4X2SDX07TP",
-};
+
 
 const app = initializeApp(firebaseConfig);
 
